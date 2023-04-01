21c58800aef451bb63943b937cd4fa58496f756c
imshubhampatel <shubhampatel2024@gmail.com>
2023-04-01 10:41:07 +0530

    added cors policies

--- app.js
+++ app.js
@@ -10,7 +10,15 @@ const PORT = process.env.PORT || 8000;
 
 //Configuring Express
 const app = express();
-app.use(cors({ origin: "http://localhost:3000" }));
+cors, {};
+app.use(
+  cors({
+    credentials: true,
+    origin: ["https://btirthorizon.in", "http://localhost:3000"],
+    "Access-Control-Allow-Origin": true,
+    "Access-Control-Allow-Methods": "PUT,GET,POST,DELETE,PATCH",
+  })
+);
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
